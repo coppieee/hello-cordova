c3d0f94c5460d295ca07d4fe677b5aa59b56033c
Daisuke Yagi <yagi_daisuke@mindfree.jp>
2015-12-07 21:57:34 +0900

    README.md修正。

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 #hello-corodova
 ##cordovaのセットアップ
-(https://gist.github.com/coppieee/9021abdca5178bf92e50)[cordova setup memo]参照
+[cordova setup memo](https://gist.github.com/coppieee/9021abdca5178bf92e50)参照
 
 ##プロジェクトのスタートの仕方
 `Donwload ZIP`ボタンからダウンロードするか、コマンドで`git clone git@github.com:coppieee/hello-cordova.git`でダウンロードする。
@@ -14,3 +14,6 @@ $ npm start
 ```
 で実行できる。`npm insatll`は実行に必要なモジュールをインストールするコマンドで初回の一回のみでOK。
 次回以降は`npm start`だけでOK。
+
+##v0.1プロジェクトの実行
+##v0.2ページのアニメーション
--- www/js/index.js
+++ www/js/index.js
@@ -1,15 +1,15 @@
 (function($){
-    'use strict';
-    document.addEventListener('deviceready',function(){
-        $(function(){
-        	$('.button').click(function(){
-        		var $a = $(this);
-        		var id = $a.attr('data-to');
-        		var $target = $(id);
-        		$target.find('~.show').removeClass('show');
+	'use strict';
+	document.addEventListener('deviceready',function(){
+		$(function(){
+			$('.button').click(function(){
+				var $a = $(this);
+				var id = $a.attr('data-to');
+				var $target = $(id);
+				$target.find('~.show').removeClass('show');
 
-        		$(id).addClass('show');
-        	});
-        });
-    }, false);
+				$(id).addClass('show');
+			});
+		});
+	}, false);
 })(jQuery);
