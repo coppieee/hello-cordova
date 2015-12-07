06df148f707e791c545d7adb1fd2a4d1543077cd
Daisuke Yagi <yagi_daisuke@mindfree.jp>
2015-12-07 21:46:43 +0900

    .pageの遷移をアニメーションさせるように修正。

--- www/js/index.js
+++ www/js/index.js
@@ -2,7 +2,14 @@
     'use strict';
     document.addEventListener('deviceready',function(){
         $(function(){
+        	$('.button').click(function(){
+        		var $a = $(this);
+        		var id = $a.attr('data-to');
+        		var $target = $(id);
+        		$target.find('~.show').removeClass('show');
 
+        		$(id).addClass('show');
+        	});
         });
     }, false);
 })(jQuery);
